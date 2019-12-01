a3b1ee366f46d89f70e4ece08725ee10253e90bd
Vladimir Babin <vovababin@gmail.com>
2019-12-01 22:28:53 +0500

    Add shortcut for save event

--- public/js/editor.js
+++ public/js/editor.js
@@ -86,6 +86,24 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/@codexteam/shortcuts/dist/shortcuts.js":
+/*!*************************************************************!*\
+  !*** ./node_modules/@codexteam/shortcuts/dist/shortcuts.js ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/*!
+ * Library for handling keyboard shortcuts
+ * @copyright CodeX (https://codex.so)
+ * @license MIT
+ * @author CodeX (https://codex.so)
+ * @version 1.1.1
+ */
+!function(e,t){ true?module.exports=t():undefined}(window,function(){return function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}return o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=0)}([function(e,t,n){"use strict";function r(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function o(e,t,n){return t&&r(e.prototype,t),n&&r(e,n),e}n.r(t);var u=function(){function i(e){var t=this;!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,i),this.commands={},this.keys={},this.name=e.name,this.parseShortcutName(e.name),this.element=e.on,this.callback=e.callback,this.executeShortcut=function(e){t.execute(e)},this.element.addEventListener("keydown",this.executeShortcut,!1)}return o(i,null,[{key:"supportedCommands",get:function(){return{SHIFT:["SHIFT"],CMD:["CMD","CONTROL","COMMAND","WINDOWS","CTRL"],ALT:["ALT","OPTION"]}}},{key:"keyCodes",get:function(){return{0:48,1:49,2:50,3:51,4:52,5:53,6:54,7:55,8:56,9:57,A:65,B:66,C:67,D:68,E:69,F:70,G:71,H:72,I:73,J:74,K:75,L:76,M:77,N:78,O:79,P:80,Q:81,R:82,S:83,T:84,U:85,V:86,W:87,X:88,Y:89,Z:90,BACKSPACE:8,ENTER:13,ESCAPE:27,LEFT:37,UP:38,RIGHT:39,DOWN:40,INSERT:45,DELETE:46}}}]),o(i,[{key:"parseShortcutName",value:function(e){e=e.split("+");for(var t=0;t<e.length;t++){e[t]=e[t].toUpperCase();var n=!1;for(var r in i.supportedCommands)if(i.supportedCommands[r].includes(e[t])){n=this.commands[r]=!0;break}n||(this.keys[e[t]]=!0)}for(var o in i.supportedCommands)this.commands[o]||(this.commands[o]=!1)}},{key:"execute",value:function(e){var t,n={CMD:e.ctrlKey||e.metaKey,SHIFT:e.shiftKey,ALT:e.altKey},r=!0;for(t in this.commands)this.commands[t]!==n[t]&&(r=!1);var o,u=!0;for(o in this.keys)u=u&&e.keyCode===i.keyCodes[o];r&&u&&this.callback(e)}},{key:"remove",value:function(){this.element.removeEventListener("keydown",this.executeShortcut)}}]),i}();t.default=u}]).default});
+
+/***/ }),
+
 /***/ "./node_modules/@editorjs/delimiter/dist/bundle.js":
 /*!*********************************************************!*\
   !*** ./node_modules/@editorjs/delimiter/dist/bundle.js ***!
@@ -218,6 +236,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _editorjs_delimiter__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @editorjs/delimiter */ "./node_modules/@editorjs/delimiter/dist/bundle.js");
 /* harmony import */ var _editorjs_delimiter__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_editorjs_delimiter__WEBPACK_IMPORTED_MODULE_2__);
 /* harmony import */ var _headers__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./headers */ "./resources/js/editor/headers.js");
+/* harmony import */ var _codexteam_shortcuts__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @codexteam/shortcuts */ "./node_modules/@codexteam/shortcuts/dist/shortcuts.js");
+/* harmony import */ var _codexteam_shortcuts__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_codexteam_shortcuts__WEBPACK_IMPORTED_MODULE_4__);
+
 
 
 
@@ -225,6 +246,7 @@ __webpack_require__.r(__webpack_exports__);
 var editor = new _editorjs_editorjs__WEBPACK_IMPORTED_MODULE_0___default.a({
   holderId: 'codex-editor',
   autofocus: true,
+  placeholder: 'Введите текст поста',
   tools: {
     header: {
       "class": _headers__WEBPACK_IMPORTED_MODULE_3__["default"],
@@ -239,6 +261,16 @@ var editor = new _editorjs_editorjs__WEBPACK_IMPORTED_MODULE_0___default.a({
       inlineToolbar: true
     },
     delimiter: _editorjs_delimiter__WEBPACK_IMPORTED_MODULE_2___default.a
+  }
+});
+new _codexteam_shortcuts__WEBPACK_IMPORTED_MODULE_4___default.a({
+  name: 'CMD+S',
+  on: document.body,
+  callback: function callback(event) {
+    editor.save().then(function (data) {
+      console.log("Data: ", data);
+    });
+    event.preventDefault();
   }
 });
 
--- resources/js/editor/editor.js
+++ resources/js/editor/editor.js
@@ -2,10 +2,12 @@ import EditorJS from '@editorjs/editorjs';
 import List from '@editorjs/list';
 import Delimiter from '@editorjs/delimiter';
 import HeaderSettings from './headers';
+import Shortcut from '@codexteam/shortcuts';
 
 const editor = new EditorJS({
     holderId: 'codex-editor',
     autofocus: true,
+    placeholder: 'Введите текст поста',
     tools: {
         header: {
             class: HeaderSettings,
@@ -21,4 +23,15 @@ const editor = new EditorJS({
         },
         delimiter: Delimiter,
     },
+});
+
+new Shortcut({
+    name: 'CMD+S',
+    on: document.body,
+    callback: function(event) {
+        editor.save().then(data => {
+            console.log("Data: ", data);
+        });
+        event.preventDefault();
+    }
 });
